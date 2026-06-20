4abbfd6e458ccfe5dcbb32c7c831c31207783603
HermarkDevv <tiamiyuhammedk001@gmail.com>
2026-06-20 19:44:59 +0100

    first commit

---
+++ src/app/(dashboard)/help-center/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { HelpCenter } from '@/app/(dashboard)/help-center/components/HelpCenter'
+
+export default function Page() {
+    return (
+        <HelpCenter />
+    )
+}
